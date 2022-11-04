89b0d1f5d4808157e3558c7b832f09377fce5cad
WanjikuMac <edwardscaroline32@gmail.com>
2022-11-04 10:14:42 +0300

    update ui web to vsn 1.6 of phoenix

--- ui/lib/ui_web/endpoint.ex
+++ ui/lib/ui_web/endpoint.ex
@@ -1,9 +1,15 @@
 defmodule UiWeb.Endpoint do
   use Phoenix.Endpoint, otp_app: :ui
+ # The session will be stored in the cookie and signed,
+  # this means its contents can be read but not tampered with.
+  # Set :encryption_salt if you would also like to encrypt it.
+  @session_options [
+    store: :cookie,
+    key: "_ui_key",
+    signing_salt: "bZArJcbY"
+  ]
 
-  socket "/socket", UiWeb.UserSocket,
-    websocket: true,
-    longpoll: false
+  socket "/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options]]
 
   # Serve at "/" the static files from "priv/static" directory.
   #
@@ -33,14 +39,6 @@ defmodule UiWeb.Endpoint do
 
   plug Plug.MethodOverride
   plug Plug.Head
-
-  # The session will be stored in the cookie and signed,
-  # this means its contents can be read but not tampered with.
-  # Set :encryption_salt if you would also like to encrypt it.
-  plug Plug.Session,
-    store: :cookie,
-    key: "_ui_key",
-    signing_salt: "bZArJcbY"
-
+  plug Plug.Session, @session_options
   plug UiWeb.Router
 end
